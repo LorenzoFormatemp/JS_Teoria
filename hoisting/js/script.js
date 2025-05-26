013de46c56ec5aac18120acfbe9c47016bde57d6
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-26 12:30:34 +0200

    add hoisting

---
+++ hoisting/js/script.js
@@ -0,0 +1,25 @@
+/* 
+    HOISTING
+
+*/
+
+saluta();
+
+function saluta(){
+    console.log('saluto');
+}
+
+console.log(x); // undefined
+var x = 10;
+
+console.log(y); // errore
+let y = 5;
+
+
+
+
+
+
+
+
+
