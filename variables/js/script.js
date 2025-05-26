6c0d833e98296655d4804034b397d051639817a0
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-26 09:53:11 +0200

    variables data types

--- variables/js/script.js
+++ variables/js/script.js
@@ -19,5 +19,58 @@ esempio1 = 'Testo';
 esempio1 = 12;
 esempio1 = null;
 
-console.log(variable);
+console.log(variable); // qui avrò errore
 let variable;
+
+// 4. VAR
+    // comportamento simile a let
+var number = 2;
+number = 10;
+
+var string;
+string = 'testo';
+
+console.log(newVar); // undefined
+var newVar = 140;
+
+/* 
+    5. Come chiamare le variabili? 
+*/
+
+//const 1ciao = 'agfa'; => no numeri iniziali
+const nuova_variabile = '...'; // OK
+const nuovaVariabile = '....'; // OK
+//const #variable = '...' // => evitare caratteri particolari
+/* let let;
+const const; */  // => no parole chiave
+
+/* 
+    Tipi di dato in JS
+*/
+
+// strings
+let string = 'stringa'; let string1 = '';// anche il vuoto è una stringa
+
+// number
+let number = 10.5; // numeri: interi e decimali
+
+// boolean: v/f
+let bool1 = true;
+let bool2 = false;
+
+// undefined
+let defined;
+console.log(defined); // trova la variabile, ma non ha valore, perciò la legge come undefined
+// null
+let valore_nullo = null;
+
+// object
+const obj = {
+    chiave1: 'valore1',
+    chiave2: 10,
+    saluta: function saluto(){
+    }
+}
+
+// function
+function fun1(){};
