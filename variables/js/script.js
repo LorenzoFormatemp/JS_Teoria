3cf2cbcfe6a34fe6522fe462e14cc69f3df6a2fc
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-26 09:26:00 +0200

    const

---
+++ variables/js/script.js
@@ -0,0 +1,12 @@
+// 1. Dichiarazione variabile:  (parola chiave) + nomeVariabile
+const variabile1 = 'valore';
+let variabile2;
+var variabile3;
+
+// 2. CONST
+    // non riassegnabile
+    // nel momento in cui si dichiara le va assegnato un valore
+
+//const esempio; //TypeError
+const esempio = 'Esempio'; 
+esempio = 'Esempio1'; //TypeError
