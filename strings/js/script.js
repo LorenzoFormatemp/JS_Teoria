5b8ff89799cde107d3888e92cc96fe8c9023dfdc
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-26 10:01:52 +0200

    strings

---
+++ strings/js/script.js
@@ -0,0 +1,14 @@
+/* 
+    stringa => sequenza di caratteri all'interno di apici o doppi apici
+*/
+const string = "stringa 1";
+const string1 = 'stringa 2';
+
+// somma stringhe
+const string3 = string + " " + string1; 
+
+// virgolette => differenziano da numero o altri tipi di dato
+const string4 = 10; // numero
+const string5 = '10'; // stringa
+
+const string6 = string4 + string5; // esempio di conversione implicita
