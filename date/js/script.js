e39a1e8cfb96821845c84efc3822a46f7845832f
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-26 12:00:19 +0200

    add random js

--- date/js/script.js
+++ date/js/script.js
@@ -1,20 +1,8 @@
 /* 
     Date in js
 */
-
 const now = new Date();
 console.log(now);
 console.log(now.getDay());
 console.log(now.getHours());
 
-
-/* 
-    Classe: veicolo
-
-        // proprietà: marca, cilindrata, colore
-        // metodi: accelera(), ...
-
-    Oggetti: copia 'singola' della classe da cui deriva
-        const bicicletta = new Veicolo();
-        const macchina = new Veicolo();
-*/
