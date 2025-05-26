0bfafa74dfb94ca1774b6f937c19008fbe1f6112
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-26 12:21:18 +0200

    scope

--- scope/js/script.js
+++ scope/js/script.js
@@ -1,9 +1,10 @@
 /* 
-    scope globale:
+    1. scope globale:
         - variabili accessibili ovunque nel codice
         - fuori da ogni blocco
+    2. scope blocco 
 */
-const containerElement = document.getElementById('main_container');
+/* const containerElement = document.getElementById('main_container');
 
 let myVar = 'Scope globale';
 
@@ -15,7 +16,20 @@ if(true){
 }
 
 console.log(myVar);
-console.log(blockVar);
+console.log(blockVar); */
 
+let globalVar = 'Sono una variabile globale';
 
+function miaFunzione(){ // 3. scope di funzione
+    let funzioneVar = 'Mi trovo dentro la funzione';
+    console.log(funzioneVar);
+    console.log(globalVar);
+
+    return funzioneVar;
+}
+
+const valoreDiFunzione = miaFunzione();
+
+console.log(globalVar);
+console.log(funzioneVar);
 
