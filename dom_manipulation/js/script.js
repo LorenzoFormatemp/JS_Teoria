7051d1188e7a3d71918905d54e7bc8595961f94f
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-27 09:53:42 +0200

    add dom manipulation

---
+++ dom_manipulation/js/script.js
@@ -0,0 +1,12 @@
+// seleziono elemento tramite id
+const divEl = document.getElementById('element');
+
+// elenco classi
+console.log(divEl.classList);
+
+// imposto una classe
+divEl.className = 'container';
+
+// elenco classi
+console.log(divEl.classList);
+
